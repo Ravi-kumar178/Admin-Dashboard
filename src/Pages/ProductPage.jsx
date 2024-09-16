2fc9aadded686875a460047f0312069bd48a3993
Ravi <bhardwajravi2025@gmail.com>
2024-09-17 00:49:56 +0530

    admin-dashboard

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -3,6 +3,10 @@ import Header from '../Components/Common/Header'
 import { motion } from 'framer-motion'
 import { AlertTriangle, DollarSign, Package, TrendingUp } from 'lucide-react'
 import StatCard from '../Components/Common/StatCard'
+import CategoryDistribution from '../Components/Overview/CategoryDistribution'
+import SalesTrendChart from '../Components/Products/SalesTrendChart'
+import ProductTable from '../Components/Products/ProductTable'
+
 
 
 const ProductPage = () => {
@@ -23,7 +27,17 @@ const ProductPage = () => {
                 <StatCard name={"Low Stock"} icon={AlertTriangle} value={23} colors={"#F59E0B"}/>
                 <StatCard name={"Total Revenue"} icon={DollarSign} value={"$543,210"} colors={"#EF4444"}/>
             </motion.div>
+
+            {/* product table */}
+             <ProductTable/>
+            {/* charts */}
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 '>
+                <SalesTrendChart/>
+                <CategoryDistribution/>
+            </div>
         </main>
+
+        
 
     </div>
   )
